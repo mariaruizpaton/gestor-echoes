deb6790e7914e7de0993188bc8cb3634c2897cd1
Liviu <ldrotariu01@gmail.com>
2026-03-10 17:47:42 +0100

    [MOD] Models

---
+++ Models/EchoModels.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Usuario
+{
+    [BsonId]
+    public string Username { get; set; } // ID Único
+    public string Email { get; set; }
+    public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+    public string Bio { get; set; }
+    public List<string> Siguiendo { get; set; } = new List<string>();
+}
+
+public class Echo
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Autor { get; set; }
+    public string Contenido { get; set; }
+    public DateTime FechaPublicacion { get; set; } = DateTime.UtcNow;
+    public int Likes { get; set; } = 0;
+    public List<string> Multimedia { get; set; } = new List<string>();
+}
